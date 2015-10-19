e211c6a1cdd75dd61e7fabf5420d7c513ec4a98c
José Victor Hipólito Jara Barrientos <vicjohi@gmail.com>
2015-10-19 14:07:38 -0500

    Creando el proyecto RRHH-modelo

---
+++ rrhh-modelo/src/main/java/com/inkadroid/Usuario.java
@@ -0,0 +1,77 @@
+package com.inkadroid;
+/**
+ * 
+ */
+
+/**
+ * @author José
+ *
+ */
+public class Usuario {
+	private String usuario;
+	private String contrasenia;
+	private String nombres;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return usuario;
+	}
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	/**
+	 * @return the contrasenia
+	 */
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	/**
+	 * @param contrasenia the contrasenia to set
+	 */
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	/**
+	 * @return the nombres
+	 */
+	public String getNombres() {
+		return nombres;
+	}
+	/**
+	 * @param nombres the nombres to set
+	 */
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	/**
+	 * @return the apellidoPaterno
+	 */
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	/**
+	 * @param apellidoPaterno the apellidoPaterno to set
+	 */
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	/**
+	 * @return the apellidoMaterno
+	 */
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	/**
+	 * @param apellidoMaterno the apellidoMaterno to set
+	 */
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+}
